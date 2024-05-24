2d3b58a36d2fb4a4dd84319421610ca63de6ae8c
jerrybowang <jerrybowang@icloud.com>
2024-05-23 21:07:58 -0700

    add negate to the logic

--- demo.sh
+++ demo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-branch="feature/*/test/*/**/new/updates"
+branch="feature/*/test/*/**/(new)/{updates}/[*]"
 
 # 第一步转换
-regex=$(echo "$branch" | sed 's/\./\\./g; s/\*/[^\/]*/g; s/\?/.{1}/g')
+regex=$(echo "$branch" | sed "s/[][(){}]/\\\\&/g")
+regex=$(echo "$regex" | sed ' s/\./\\./g; s/\*/[^\/]*/g; s/\?/.{1}/g')
 
 # 第二步转换，将连续的 `[^/]*[^/]*` 替换为 `.*`
 regex=$(echo "$regex" | sed 's/\[\^\/\]\*\[\^\/\]\*/.*/g')
--- test-regrex.sh
+++ test-regrex.sh
@@ -3,13 +3,23 @@
 # function to convert branch name to regex
 convert_to_regex() {
     local branch=$1
-    local regex=$(echo "$branch" | sed 's/\./\\./g; s/\*/[^\/]*/g; s/\?/.{1}/g')
+    regex=$(echo "$branch" | sed "s/[(){}+|]/\\\\&/g")
+    regex=$(echo "$regex" | sed 's/\./\\./g; s/\*/[^\/]*/g; s/\?/.{1}/g')
     regex=$(echo "$regex" | sed 's/\[\^\/\]\*\[\^\/\]\*/.*/g')
     echo "$regex"
 }
 
 # test branches
-test_branches=("feature/new" "feature/new/updates" "bugfix" "bugfix/urgent" "main" "test/main")
+test_branches=("feature/new"
+                "feature/new/updates" 
+                "bugfix" "bugfix/urgent" 
+                "main" "test/main" "test-e" 
+                "test/OPS-201]-test" "f/(test)" 
+                "f/{test}/(new)" "f/{test}/(new)/{updates}"
+                "f/{test}/(new)/{updates}/d"
+                "a-test" "fix/OPS-301" "fix/OPS-291/test"
+                "a|b" "a+b" "a(b)" "a{b}" "ab]"
+                )
 
 # input branch pattern
 read -p "Enter the branch pattern: " branch_pattern
@@ -21,9 +31,21 @@ echo "Regex pattern is: $regex"
 
 # match test branches
 for test_branch in "${test_branches[@]}"; do
+    negate=false
+    # if there is a negte character "!"
+    if [[ $regex =~ ^! ]]; then
+        # remove the "!" character
+        regex=${regex:1}
+        negate=true
+    fi
+
     if [[ $test_branch =~ ^$regex$ ]]; then
         echo "Matches \"$test_branch\""
     else
-        echo "Does not match \"$test_branch\""
+        if $negate; then
+            echo "Matches \"$test_branch\""
+        else
+            echo "Does not match \"$test_branch\""
+        fi
     fi
 done
